047a1416086f9622fddc9700cdb9714ed1839757
Yash Lalwani <yash.lalwani10@outlook.com>
2026-01-07 12:59:27 -0500

    Postorder Traversal in Binary Tree O(n)

---
+++ dsa/binarytrees/PostorderTraversal.java
@@ -0,0 +1,68 @@
+public class PostorderTraversal {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node (int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTrees {
+        static int idx = -1;
+
+        public static Node buildTree (int[] nodes) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        // Time complexity for preorder traversal is O(n) because we went through n nodes once.
+        public static void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        // Time complexity for inorder traversal is linear = O(n)
+        public static void inorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+        // Time complexity for postorder traversal is again linear = O(n)
+        public static void postorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+
+        }
+    }
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTrees tree = new BinaryTrees();
+        Node root = tree.buildTree(nodes);
+
+        tree.postorder(root);
+    }
+}
