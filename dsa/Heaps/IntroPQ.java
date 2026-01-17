b60045f24bf3257109c861a94e79aa80b5109b5d
Yash Lalwani <yash.lalwani10@outlook.com>
2026-01-16 19:05:18 -0500

    Intro to priority queue

---
+++ dsa/Heaps/IntroPQ.java
@@ -0,0 +1,24 @@
+package Heaps;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class IntroPQ {
+    public static void main(String args[]) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        // --> if we want to reverse the order
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(3); //O(logn)
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+
+        // The remove will not follow FIFO like queue, becuase it's priority queue now
+        while(!pq.isEmpty()) {
+            System.out.println(pq.peek()); //O(1)
+            pq.remove(); //O(logn)
+        }
+    }
+}
