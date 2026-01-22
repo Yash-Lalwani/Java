77278d179fbe6c70e748eefadf0a1742e7f3a0e6
Yash Lalwani <yash.lalwani10@outlook.com>
2026-01-22 13:30:00 -0500

    To find max Subarray sum using Kadane's Algorithm

---
+++ dsa/Cram-50/Arrays/KadanesAlgorithm.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class KadanesAlgorithm {
+    
+    public static void kadanes(int arr[]) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0; 
+        
+        for (int n : arr) {
+            if (sum < 0) {
+                sum = 0;
+            }
+            sum += n;
+            max = Math.max(sum,max);
+        }
+        System.out.println("our max sum is: " + max);
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {-2, -3, 4, -1, -2 , 1, 5 ,-3};
+        kadanes(numbers);
+    }
+}
