8b57837058c88874900c0b93fceb7eb13d7a7436
Yash Lalwani <yash.lalwani10@outlook.com>
2026-06-07 20:44:18 -0400

    ClimbingStairs tabulation approach

--- dsa/DP_1/ClimbingStairs_Tabulation.java
+++ dsa/DP_1/ClimbingStairs_Tabulation.java
@@ -1,5 +1,25 @@
 package DP_1;
 
 public class ClimbingStairs_Tabulation {
-    
+
+    public static int countWays(int n) {
+        int dp[] = new int[n + 1];
+
+        // Base cases
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // Fill from bottom-up
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+
+        System.out.println(countWays(n));
+    }
 }
