d919a70bec967f7758a78ce34a255fadddd6eddb
Yash Lalwani <yash.lalwani10@outlook.com>
2026-06-07 20:14:46 -0400

    Climbing stairs code recurssion

--- dsa/DP_1/DP_Fib_Tabulation.java
+++ dsa/DP_1/DP_Fib_Tabulation.java
@@ -7,7 +7,7 @@ public class DP_Fib_Tabulation {
     public static int fibTabulation(int n) {
         int dp[] = new int[n + 1];
 
-        dp[0] = 0;
+        dp[0] = 0; // not really needed to do, cuz it's already 0. just so that i can vizualize.
         dp[1] = 1;
 
         for (int i = 2; i <= n; i++) {
